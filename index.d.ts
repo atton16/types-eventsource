a338c07041347b78b7dda24ad05791dd9eefe22e
Attawit Kittikrairit <atton16@gmail.com>
2017-11-13 00:28:19 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,14 @@
+// 
+// Type definitions for EventSource (https://github.com/EventSource/eventsource).
+// 
+// Created by Attawit Kittikrairit
+// 
+// Original type definitions file from (https://github.com/sguiheux/EventSource/blob/master/typings.d.ts).
+// 
+
+export = EventSource;
+
 declare enum ReadyState {CONNECTING = 0, OPEN = 1, CLOSED = 2}
-
-export default class EventSource extends EventTarget {
-  CONNECTING: ReadyState;
-  OPEN: ReadyState;
-  CLOSED: ReadyState;
-
-  constructor(url: string, eventSourceInitDict?: EventSourceInitDict);
-  url: string;
-  readyState: ReadyState;
-  onopen: EventListenerOrEventListenerObject;
-  onmessage: EventListenerOrEventListenerObject;
-  onerror: EventListenerOrEventListenerObject;
-  close: () => void;
-}
 
 interface EventSourceInitDict {
   withCredentials?: boolean;
@@ -20,4 +16,21 @@ interface EventSourceInitDict {
   proxy?: string;
   https?: Object;
   rejectUnauthorized?: boolean; // Backward compatible. Recommend to use https.rejectUnauthorized instead
+}
+
+declare class EventSource extends EventTarget {
+  static CLOSED: ReadyState;
+  static CONNECTING: ReadyState;
+  static OPEN: ReadyState;
+
+  url: string;
+  readyState: ReadyState;
+  onopen: EventListenerOrEventListenerObject;
+  onmessage: EventListenerOrEventListenerObject;
+  onerror: EventListenerOrEventListenerObject;
+
+  constructor(url: string, eventSourceInitDict?: EventSourceInitDict);
+
+  close(): void;
+  
 }
